bb68fd1e3d0ced804fdef0295ad3a42d9d0e6cb0
Michael A. Alcorn <airalcorn2@gmail.com>
2019-01-05 11:49:49 -0600

    Formatting.

--- ranknet.py
+++ ranknet.py
@@ -12,20 +12,20 @@ from keras.models import Model
 INPUT_DIM = 50
 
 # Model.
-h_1 = Dense(128, activation = "relu")
-h_2 = Dense(64, activation = "relu")
-h_3 = Dense(32, activation = "relu")
+h_1 = Dense(128, activation="relu")
+h_2 = Dense(64, activation="relu")
+h_3 = Dense(32, activation="relu")
 s = Dense(1)
 
 # Relevant document score.
-rel_doc = Input(shape = (INPUT_DIM, ), dtype = "float32")
+rel_doc = Input(shape=(INPUT_DIM,), dtype="float32")
 h_1_rel = h_1(rel_doc)
 h_2_rel = h_2(h_1_rel)
 h_3_rel = h_3(h_2_rel)
 rel_score = s(h_3_rel)
 
 # Irrelevant document score.
-irr_doc = Input(shape = (INPUT_DIM, ), dtype = "float32")
+irr_doc = Input(shape=(INPUT_DIM,), dtype="float32")
 h_1_irr = h_1(irr_doc)
 h_2_irr = h_2(h_1_irr)
 h_3_irr = h_3(h_2_irr)
@@ -38,19 +38,19 @@ diff = Subtract()([rel_score, irr_score])
 prob = Activation("sigmoid")(diff)
 
 # Build model.
-model = Model(inputs = [rel_doc, irr_doc], outputs = prob)
-model.compile(optimizer = "adadelta", loss = "binary_crossentropy")
+model = Model(inputs=[rel_doc, irr_doc], outputs=prob)
+model.compile(optimizer="adadelta", loss="binary_crossentropy")
 
 # Fake data.
 N = 100
-X_1 = 2 * np.random.uniform(size = (N, INPUT_DIM))
-X_2 = np.random.uniform(size = (N, INPUT_DIM))
+X_1 = 2 * np.random.uniform(size=(N, INPUT_DIM))
+X_2 = np.random.uniform(size=(N, INPUT_DIM))
 y = np.ones((X_1.shape[0], 1))
 
 # Train model.
 NUM_EPOCHS = 10
 BATCH_SIZE = 10
-history = model.fit([X_1, X_2], y, batch_size = BATCH_SIZE, epochs = NUM_EPOCHS, verbose = 1)
+history = model.fit([X_1, X_2], y, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS, verbose=1)
 
 # Generate scores from document/query features.
 get_score = backend.function([rel_doc], [rel_score])
